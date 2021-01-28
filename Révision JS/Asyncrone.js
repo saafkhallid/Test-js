3a3c732d1561006bfc29be26f1236a7357bad20f
khalid saaf <khallid.saaf@gmail.com>
2021-01-28 21:29:12 +0100

    ajouter code asyncrone

---
+++ Révision JS/Asyncrone.js
@@ -0,0 +1,9 @@
+
+
+console.log("Premier function");
+
+window.setTimeout(()=>console.log("Get Friends later"),1000);
+window.setTimeout(()=>console.log("Get Friends later"),2000);
+
+
+console.log("deuxiemme function");
